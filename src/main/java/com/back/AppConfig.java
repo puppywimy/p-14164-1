c1df248a6b27ec6da808deaf3d919174de1e7828
puppywimy <eric250@naver.com>
2026-05-12 08:32:53 +0900

    9강: @Bean보다 @Component를 사용하도록 수정

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,27 +1,13 @@
 
 package com.back;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
     @Bean
     int version() {
         return 55;
-    }
-
-    @Bean
-    @Order(0)
-    public ApplicationRunner myApplicationRunner() {
-        return new MyApplicationRunner();
-    }
-
-    @Bean
-    @Order(1)
-    public ApplicationRunner myApplicationRunner2() {
-        return new MyApplicationRunner2();
     }
 }
